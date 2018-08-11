f3936a57e5945c6c15e5f9c39ea001105a08093a
Volcomix <volcomix@gmail.com>
2018-08-11 16:32:28 +0200

    Reorganize charts into cards

--- src/containers/Learning.js
+++ src/containers/Learning.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import classNames from 'classnames'
 import {
   ResponsiveContainer,
   LineChart,
@@ -23,10 +25,20 @@ const margin = { left: 24, right: 42, bottom: 24 }
 
 const styles = theme => ({
   container: {
-    marginTop: theme.spacing.unit * 11,
+    marginTop: theme.spacing.unit * 10,
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  card: {
+    marginTop: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit * 2,
   },
   label: {
     ...theme.typography.body1,
+  },
+  inputs: {
+    paddingBottom: theme.spacing.unit * 2,
   },
   historyContainer: {
     display: 'flex',
@@ -42,51 +54,59 @@ const styles = theme => ({
     marginLeft: theme.spacing.unit * 10,
     marginBottom: theme.spacing.unit,
   },
+  brush: {
+    transform: `translateY(${theme.spacing.unit * 3}px)`,
+  },
 })
 
 const Learning = ({ classes, inputs, data, history }) => (
   <div className={classes.container}>
-    {inputs && [
-      <ResponsiveContainer key="features" height={chartHeight}>
-        <LineChart data={inputs} margin={margin} syncId="inputs">
-          <CartesianGrid vertical={false} />
-          <Tooltip isAnimationActive={false} />
-          {Object.keys(inputs[0])
-            .filter(j => !['date', 'target'].includes(j))
-            .map((j, i) => (
-              <Line key={j} dataKey={j} dot={false} stroke={chartColors(i)} />
-            ))}
-          <XAxis dataKey="date" hide={true} />
-          <YAxis />
-        </LineChart>
-      </ResponsiveContainer>,
-      <ResponsiveContainer key="target" height={chartHeight}>
-        <BarChart data={inputs} margin={margin} syncId="inputs">
-          <CartesianGrid vertical={false} />
-          <Tooltip isAnimationActive={false} />
-          <Bar dataKey="target" dot={false} fill={chartColors(0)} />
-          <XAxis dataKey="date" hide={true} />
-          <YAxis ticks={[-1, 0, 1]} />
-          <Brush />
-        </BarChart>
-      </ResponsiveContainer>,
-    ]}
-    {data &&
-      data.map((chart, i) => (
-        <ResponsiveContainer key={i} height={chartHeight}>
-          <LineChart data={chart} margin={margin} syncId="data">
+    {inputs && (
+      <Paper className={classNames(classes.card, classes.inputs)}>
+        <ResponsiveContainer height={chartHeight}>
+          <LineChart data={inputs} margin={margin} syncId="inputs">
             <CartesianGrid vertical={false} />
             <Tooltip isAnimationActive={false} />
-            {Object.keys(chart[0]).map(j => (
-              <Line key={j} dataKey={j} dot={false} stroke={chartColors(j)} />
-            ))}
+            {Object.keys(inputs[0])
+              .filter(j => !['date', 'target'].includes(j))
+              .map((j, i) => (
+                <Line key={j} dataKey={j} dot={false} stroke={chartColors(i)} />
+              ))}
+            <XAxis dataKey="date" hide={true} />
             <YAxis />
           </LineChart>
         </ResponsiveContainer>
-      ))}
+        <ResponsiveContainer height={chartHeight}>
+          <BarChart data={inputs} margin={margin} syncId="inputs">
+            <CartesianGrid vertical={false} />
+            <Tooltip isAnimationActive={false} />
+            <Bar dataKey="target" dot={false} fill={chartColors(0)} />
+            <XAxis dataKey="date" hide={true} />
+            <YAxis ticks={[-1, 0, 1]} />
+            <Brush className={classes.brush} />
+          </BarChart>
+        </ResponsiveContainer>
+      </Paper>
+    )}
+    {data && (
+      <Paper className={classes.card}>
+        {data.map((chart, i) => (
+          <ResponsiveContainer key={i} height={chartHeight}>
+            <LineChart data={chart} margin={margin} syncId="data">
+              <CartesianGrid vertical={false} />
+              <Tooltip isAnimationActive={false} />
+              {Object.keys(chart[0]).map(j => (
+                <Line key={j} dataKey={j} dot={false} stroke={chartColors(j)} />
+              ))}
+              <YAxis />
+            </LineChart>
+          </ResponsiveContainer>
+        ))}
+      </Paper>
+    )}
     {history &&
       history.length && (
-        <div className={classes.historyContainer}>
+        <Paper className={classNames(classes.card, classes.historyContainer)}>
           <div className={classes.history}>
             <Typography className={classes.historyLabel}>
               Last loss: {history[history.length - 1].loss.toFixed(2)}
@@ -146,7 +166,7 @@ const Learning = ({ classes, inputs, data, history }) => (
               </LineChart>
             </ResponsiveContainer>
           </div>
-        </div>
+        </Paper>
       )}
   </div>
 )

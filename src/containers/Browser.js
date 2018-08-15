3c2d305288f71438c2624e8791ae14fac0d7974b
Volcomix <volcomix@gmail.com>
2018-08-15 17:54:05 +0200

    Improve browser status card with status colors

--- src/containers/Browser.js
+++ src/containers/Browser.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+import green from '@material-ui/core/colors/green'
 
 import { startBrowser } from '../actions/browser'
 
@@ -19,38 +20,51 @@ const styles = theme => ({
       flexGrow: 1,
     },
   },
+  started: {
+    color: green['A700'],
+  },
+  stopped: {
+    color: theme.palette.error.main,
+  },
+  unknown: {
+    color: theme.palette.text.secondary,
+  },
 })
 
-const Browser = ({ classes, isConnected, isStarted, onStartBrowser }) => {
-  let status
-  if (isStarted === true) {
-    status = 'Started'
-  } else if (isStarted === false) {
-    status = 'Stopped'
-  } else {
-    status = 'Unknown'
-  }
-  return (
-    <div className={classes.container}>
-      <Card className={classes.status}>
-        <CardContent>
-          <Typography variant="caption">Backend browser status</Typography>
-          <Typography variant="title">{status}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button
-            size="small"
-            color="primary"
-            disabled={!isConnected || isStarted}
-            onClick={onStartBrowser}
-          >
-            Start
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
-  )
-}
+const Browser = ({ classes, isConnected, isStarted, onStartBrowser }) => (
+  <div className={classes.container}>
+    <Card className={classes.status}>
+      <CardContent>
+        <Typography variant="caption">Backend browser status</Typography>
+        {isStarted === true && (
+          <Typography variant="title" className={classes.started}>
+            Started
+          </Typography>
+        )}
+        {isStarted === false && (
+          <Typography variant="title" className={classes.stopped}>
+            Stopped
+          </Typography>
+        )}
+        {isStarted === undefined && (
+          <Typography variant="title" className={classes.unknown}>
+            Unknown
+          </Typography>
+        )}
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          color="primary"
+          disabled={!isConnected || isStarted}
+          onClick={onStartBrowser}
+        >
+          Start
+        </Button>
+      </CardActions>
+    </Card>
+  </div>
+)
 
 const mapStateToProps = ({ dryMoose, browser }) => ({
   isConnected: dryMoose.isConnected,

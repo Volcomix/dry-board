398851be2ba75d7098afe9fa1ff245d7e10806a1
Volcomix <volcomix@gmail.com>
2018-08-15 12:58:48 +0200

    Add middleware index

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import { Provider } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducer from './reducers'
-import dryMoose from './middleware/dryMoose'
-import browser from './middleware/browser'
+import middleware from './middleware'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(dryMoose, browser)),
+  composeWithDevTools(applyMiddleware(...middleware)),
 )
 
 ReactDOM.render(
---
+++ src/middleware/index.js
@@ -0,0 +1,4 @@
+import dryMoose from './dryMoose'
+import browser from './browser'
+
+export default [dryMoose, browser]

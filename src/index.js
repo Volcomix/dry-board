2cd8a011b673f14d2f288ad06a292384e87af1af
Volcomix <volcomix@gmail.com>
2018-08-15 11:16:36 +0200

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducer from './reducers'
 import dryMoose from './middleware/dryMoose'
@@ -9,7 +10,10 @@ import browser from './middleware/browser'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(reducer, applyMiddleware(dryMoose, browser))
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(dryMoose, browser)),
+)
 
 ReactDOM.render(
   <Provider store={store}>

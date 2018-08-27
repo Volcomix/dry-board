16552d865a9438e83beadd13c89588a2f6754b69
Volcomix <volcomix@gmail.com>
2018-08-27 10:45:37 +0200

    Style active menu

--- src/containers/DrawerContent.js
+++ src/containers/DrawerContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -9,7 +10,16 @@ import Divider from '@material-ui/core/Divider'
 import Icon from '@material-ui/core/Icon'
 import Typography from '@material-ui/core/Typography'
 
-const DrawerContent = ({ onClose }) => (
+const styles = theme => ({
+  active: {
+    '& $item': {
+      color: theme.palette.primary.main,
+    },
+  },
+  item: {},
+})
+
+const DrawerContent = ({ classes, onClose }) => (
   <React.Fragment>
     <Toolbar>
       <Typography variant="title" color="textSecondary">
@@ -18,14 +28,23 @@ const DrawerContent = ({ onClose }) => (
     </Toolbar>
     <Divider />
     <List>
-      <ListItem button component={Link} to="/browser" onClick={onClose}>
-        <ListItemIcon>
+      <ListItem
+        button
+        component={NavLink}
+        to="/browser"
+        onClick={onClose}
+        activeClassName={classes.active}
+      >
+        <ListItemIcon className={classes.item}>
           <Icon>web</Icon>
         </ListItemIcon>
-        <ListItemText primary="Backend browser" />
+        <ListItemText
+          primary="Backend browser"
+          primaryTypographyProps={{ className: classes.item }}
+        />
       </ListItem>
     </List>
   </React.Fragment>
 )
 
-export default DrawerContent
+export default withStyles(styles)(DrawerContent)

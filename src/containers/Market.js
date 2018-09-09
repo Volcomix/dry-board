7fcb777588a6473f4f1069e3775475da67ab18a4
Volcomix <volcomix@gmail.com>
2018-09-09 12:09:11 +0200

    Init market refactoring for config

--- src/containers/Market.js
+++ src/containers/Market.js
@@ -2,47 +2,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'recompose'
 import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import StoppedIcon from '@material-ui/icons/Warning'
-import DiscoveredIcon from '@material-ui/icons/CheckCircle'
-import CancelledIcon from '@material-ui/icons/Cancel'
-import classNames from 'classnames'
+import CancelledIcon from '@material-ui/icons/Close'
+import DiscoveredIcon from '@material-ui/icons/Check'
 
 import { Status as EToroStatus } from '../reducers/eToro'
 import { Status as MarketStatus } from '../reducers/market'
 import { discoverMarket, cancelMarketDiscovery } from '../actions/market'
 
-const iconSize = 32
-
 const styles = theme => ({
-  container: {
-    display: 'flex',
+  cancelled: {
+    backgroundColor: theme.palette.error.main,
   },
-  card: {
-    [theme.breakpoints.down('xs')]: {
-      flexGrow: 1,
-    },
+  discovered: {
+    backgroundColor: theme.palette.primary.main,
   },
-  content: {
-    marginTop: theme.spacing.unit,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  icon: {
-    fontSize: iconSize,
-    marginRight: theme.spacing.unit,
-  },
-  label: {
-    flexGrow: 1,
-  },
-  disabled: {
-    color: theme.palette.text.disabled,
+  loading: {
+    backgroundColor: 'inherit',
   },
 })
 
@@ -79,63 +62,63 @@ const Market = ({
   onDiscover,
   onCancel,
 }) => (
-  <div className={classes.container}>
-    <Card className={classes.card}>
-      <CardContent className={classes.content}>
+  <Grid container>
+    <Grid item xs={12} sm={5} lg={3}>
+      <Card>
         {marketStatus === MarketStatus.Stopped && (
-          <React.Fragment>
-            <StoppedIcon color="disabled" className={classes.icon} />
-            <Typography
-              variant="headline"
-              className={classNames(classes.label, classes.disabled)}
-            >
-              Stopped
-            </Typography>
-          </React.Fragment>
+          <CardHeader avatar={<Avatar>!</Avatar>} title="Stopped" />
         )}
         {marketStatus === MarketStatus.Cancelled && (
-          <React.Fragment>
-            <CancelledIcon color="error" className={classes.icon} />
-            <Typography
-              variant="headline"
-              color="error"
-              className={classes.label}
-            >
-              Cancelled
-            </Typography>
-          </React.Fragment>
+          <CardHeader
+            avatar={
+              <Avatar className={classes.cancelled}>
+                <CancelledIcon />
+              </Avatar>
+            }
+            title="Cancelled"
+          />
         )}
         {marketStatus === MarketStatus.Discovered && (
-          <React.Fragment>
-            <DiscoveredIcon color="primary" className={classes.icon} />
-            <Typography variant="headline" className={classes.label}>
-              Discovered
-            </Typography>
-          </React.Fragment>
+          <CardHeader
+            avatar={
+              <Avatar className={classes.discovered}>
+                <DiscoveredIcon />
+              </Avatar>
+            }
+            title="Discovered"
+          />
         )}
-        {isLoading(marketStatus) && <CircularProgress size={iconSize} />}
-      </CardContent>
-      <CardActions>
-        <Button
-          variant="contained"
-          size="small"
-          color="primary"
-          disabled={!canDiscover(marketStatus, eToroStatus, isConnected)}
-          onClick={onDiscover}
-        >
-          Discover
-        </Button>
-        <Button
-          size="small"
-          color="primary"
-          disabled={!canCancel(marketStatus, eToroStatus, isConnected)}
-          onClick={onCancel}
-        >
-          Cancel
-        </Button>
-      </CardActions>
-    </Card>
-  </div>
+        {isLoading(marketStatus) && (
+          <CardHeader
+            avatar={
+              <Avatar className={classes.loading}>
+                <CircularProgress />
+              </Avatar>
+            }
+          />
+        )}
+        <CardActions>
+          <Button
+            variant="contained"
+            size="small"
+            color="primary"
+            disabled={!canDiscover(marketStatus, eToroStatus, isConnected)}
+            onClick={onDiscover}
+          >
+            Discover
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            disabled={!canCancel(marketStatus, eToroStatus, isConnected)}
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  </Grid>
 )
 
 const mapStateToProps = ({ dryMoose, eToro, market }) => ({
